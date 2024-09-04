ba4c214838d826a96fe71a0f8b78476db10947a8
Pinar Kaymaz Sengit <pinar.sengit.ext@penzilla.de>
2024-09-04 22:35:25 +0200

    add language options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,25 @@
 function switchLanguage() {
-  const language = document.getElementById('languageSwitcher').value;
+  const lang = document.getElementById('languageSwitcher').value;
 
-  if (language === 'en') {
-    document.getElementById('banner-title').innerText = 'Villa Deniz';
-    document.getElementById('banner-subtitle').innerText = 'Your Vacation Home';
-    document.getElementById('banner-location').innerText = 'Manavgat, Antalya, Türkiye';
+  fetch(`lang/${lang}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('banner-title').innerText = data.bannerTitle;
+      document.getElementById('banner-subtitle').innerText = data.bannerSubtitle;
+      document.getElementById('banner-location').innerText = data.bannerLocation;
 
-    document.getElementById('about-title').innerText = 'About';
-    document.getElementById('about-card-1').innerText = 'Dublex villa with 2 bedrooms, up to 5 guests.';
-    document.getElementById('about-card-2').innerText = 'Fully furnished. AC and WiFi is available.';
-    document.getElementById('about-card-3').innerText =
-      'Next to grocery stores and shopping centres, 300 meters to the beach, kids friendly neighbourhood.';
+      document.getElementById('about-title').innerText = data.aboutTitle;
+      document.getElementById('about-card-1').innerText = data.aboutCard1;
+      document.getElementById('about-card-2').innerText = data.aboutCard2;
+      document.getElementById('about-card-3').innerText = data.aboutCard3;
 
-    document.getElementById('photos-title').innerText = 'Photos';
+      document.getElementById('photos-title').innerText = data.photosTitle;
 
-    document.getElementById('location-title').innerText = 'Location';
+      document.getElementById('location-title').innerText = data.locationTitle;
 
-    document.getElementById('contact-title').innerText = 'Contact';
-    document.getElementById('contact-info-1').innerText = 'John Doe: +90 332 124 2332 (Turkish)';
-    document.getElementById('contact-info-2').innerText = 'Jane Doe: +49 4344 0543555 (English and German)';
-  } else if (language === 'de') {
-    document.getElementById('banner-title').innerText = 'Villa Deniz';
-    document.getElementById('banner-subtitle').innerText = 'Ihr Ferienhaus';
-    document.getElementById('banner-location').innerText = 'Manavgat, Antalya, Türkei';
-
-    document.getElementById('about-title').innerText = 'Über uns';
-    document.getElementById('about-card-1').innerText = 'Doppelhaushälfte mit 2 Schlafzimmern, für bis zu 5 Gäste.';
-    document.getElementById('about-card-2').innerText = 'Voll möbliert. Klimaanlage und WLAN verfügbar.';
-    document.getElementById('about-card-3').innerText =
-      'In der Nähe von Lebensmittelgeschäften und Einkaufszentren, 300 Meter zum Strand, kinderfreundliche Nachbarschaft.';
-
-    document.getElementById('photos-title').innerText = 'Fotos';
-
-    document.getElementById('location-title').innerText = 'Lage';
-
-    document.getElementById('contact-title').innerText = 'Kontakt';
-    document.getElementById('contact-info-1').innerText = 'John Doe: +90 332 124 2332 (Türkisch)';
-    document.getElementById('contact-info-2').innerText = 'Jane Doe: +49 4344 0543555 (Englisch und Deutsch)';
-  } else if (language === 'tr') {
-    document.getElementById('banner-title').innerText = 'Villa Deniz';
-    document.getElementById('banner-subtitle').innerText = 'Sizin Tatil Eviniz';
-    document.getElementById('banner-location').innerText = 'Manavgat, Antalya, Türkiye';
-
-    document.getElementById('about-title').innerText = 'Hakkında';
-    document.getElementById('about-card-1').innerText = '2 yatak odalı dubleks villa, 5 kişiye kadar konaklama imkanı.';
-    document.getElementById('about-card-2').innerText = 'Tam donanımlı. Klima ve WiFi mevcut.';
-    document.getElementById('about-card-3').innerText =
-      'Marketlere ve alışveriş merkezlerine yakın, plaja 300 metre mesafede, çocuk dostu mahalle.';
-
-    document.getElementById('photos-title').innerText = 'Fotoğraflar';
-
-    document.getElementById('location-title').innerText = 'Konum';
-
-    document.getElementById('contact-title').innerText = 'İletişim';
-    document.getElementById('contact-info-1').innerText = 'John Doe: +90 332 124 2332 (Türkçe)';
-    document.getElementById('contact-info-2').innerText = 'Jane Doe: +49 4344 0543555 (İngilizce ve Almanca)';
-  }
+      document.getElementById('contact-title').innerText = data.contactTitle;
+      document.getElementById('contact-info-1').innerText = data.contactInfo1;
+      document.getElementById('contact-info-2').innerText = data.contactInfo2;
+    })
+    .catch((error) => console.error('Error loading language file:', error));
 }
