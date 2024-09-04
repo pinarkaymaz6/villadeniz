4a03317235fec9823eaf3ab00fd4b1587330b512
Pinar Kaymaz Sengit <pinar.sengit.ext@penzilla.de>
2024-09-04 12:43:26 +0200

    add assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,59 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Load default language
-    const defaultLang = localStorage.getItem('language') || 'en';
-    setLanguage(defaultLang);
-});
+function switchLanguage() {
+  const language = document.getElementById('languageSwitcher').value;
 
-function setLanguage(lang) {
-    fetch(`lang/${lang}.json`)
-        .then(response => response.json())
-        .then(data => {
-            // Update text elements
-            //document.getElementById('title').textContent = data.title;
-            document.getElementById('homeLink').textContent = data.homeLink;
-            document.getElementById('contactLink').textContent = data.contactLink;
-            
-            // Check if description text exists before updating
-            const descriptionText = document.getElementById('descriptionText');
-            if (descriptionText) {
-                descriptionText.textContent = data.descriptionText;
-            }
-            
-            // Check if contact heading and text exist before updating
-            const contactHeading = document.getElementById('contactHeading');
-            const contactText = document.getElementById('contactText');
-            if (contactHeading) {
-                contactHeading.textContent = data.contactHeading;
-            }
-            if (contactText) {
-                contactText.textContent = data.contactText;
-            }
-            
-            document.getElementById('footerText').textContent = data.footerText;
+  if (language === 'en') {
+    document.getElementById('banner-title').innerText = 'Villa Deniz';
+    document.getElementById('banner-subtitle').innerText = 'Your Vacation Home';
+    document.getElementById('banner-location').innerText = 'Manavgat, Antalya, Türkiye';
 
-            // Update flag in the dropdown button
-            document.getElementById('current-flag').src = `assets/images/flags/${lang}.png`;
-        })
-        .catch(error => console.error('Error loading language file:', error));
+    document.getElementById('about-title').innerText = 'About';
+    document.getElementById('about-card-1').innerText = 'Dublex villa with 2 bedrooms, up to 5 guests.';
+    document.getElementById('about-card-2').innerText = 'Fully furnished. AC and WiFi is available.';
+    document.getElementById('about-card-3').innerText =
+      'Next to grocery stores and shopping centres, 300 meters to the beach, kids friendly neighbourhood.';
 
-    // Save selected language to local storage
-    localStorage.setItem('language', lang);
+    document.getElementById('photos-title').innerText = 'Photos';
+
+    document.getElementById('location-title').innerText = 'Location';
+
+    document.getElementById('contact-title').innerText = 'Contact';
+    document.getElementById('contact-info-1').innerText = 'John Doe: +90 332 124 2332 (Turkish)';
+    document.getElementById('contact-info-2').innerText = 'Jane Doe: +49 4344 0543555 (English and German)';
+  } else if (language === 'de') {
+    document.getElementById('banner-title').innerText = 'Villa Deniz';
+    document.getElementById('banner-subtitle').innerText = 'Ihr Ferienhaus';
+    document.getElementById('banner-location').innerText = 'Manavgat, Antalya, Türkei';
+
+    document.getElementById('about-title').innerText = 'Über uns';
+    document.getElementById('about-card-1').innerText = 'Doppelhaushälfte mit 2 Schlafzimmern, für bis zu 5 Gäste.';
+    document.getElementById('about-card-2').innerText = 'Voll möbliert. Klimaanlage und WLAN verfügbar.';
+    document.getElementById('about-card-3').innerText =
+      'In der Nähe von Lebensmittelgeschäften und Einkaufszentren, 300 Meter zum Strand, kinderfreundliche Nachbarschaft.';
+
+    document.getElementById('photos-title').innerText = 'Fotos';
+
+    document.getElementById('location-title').innerText = 'Lage';
+
+    document.getElementById('contact-title').innerText = 'Kontakt';
+    document.getElementById('contact-info-1').innerText = 'John Doe: +90 332 124 2332 (Türkisch)';
+    document.getElementById('contact-info-2').innerText = 'Jane Doe: +49 4344 0543555 (Englisch und Deutsch)';
+  } else if (language === 'tr') {
+    document.getElementById('banner-title').innerText = 'Villa Deniz';
+    document.getElementById('banner-subtitle').innerText = 'Sizin Tatil Eviniz';
+    document.getElementById('banner-location').innerText = 'Manavgat, Antalya, Türkiye';
+
+    document.getElementById('about-title').innerText = 'Hakkında';
+    document.getElementById('about-card-1').innerText = '2 yatak odalı dubleks villa, 5 kişiye kadar konaklama imkanı.';
+    document.getElementById('about-card-2').innerText = 'Tam donanımlı. Klima ve WiFi mevcut.';
+    document.getElementById('about-card-3').innerText =
+      'Marketlere ve alışveriş merkezlerine yakın, plaja 300 metre mesafede, çocuk dostu mahalle.';
+
+    document.getElementById('photos-title').innerText = 'Fotoğraflar';
+
+    document.getElementById('location-title').innerText = 'Konum';
+
+    document.getElementById('contact-title').innerText = 'İletişim';
+    document.getElementById('contact-info-1').innerText = 'John Doe: +90 332 124 2332 (Türkçe)';
+    document.getElementById('contact-info-2').innerText = 'Jane Doe: +49 4344 0543555 (İngilizce ve Almanca)';
+  }
 }
-
